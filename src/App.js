0452902ad1e4d4258f6f0ddf91fe07d32b3ba162
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-22 02:12:33 +0300

    Refactor(src/App.js) fill the parent block to full height with the codemirror

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import CodeMirror from './CodeMirror.jsx';
 // import CodeBasicEditor from './CodeBasicEditor.jsx';
 import Editor from './Editor.jsx';
+// import LivewireTemp from './LivewireTemp.jsx'
 
 const App = () => (
   <div className='container'>
     <div className='vh-100'>
-      <div className='row h-75 d-flex justify-content-center'>
-        <div className="col-6 card px-0 mt-5 bg-light">
+      <div className='row h-75 justify-content-center'>
+        <div className="col-6 card px-0 mt-5">
           <ul className="nav nav-tabs justify-content-center" id="myTab" role="tablist">
             <li className="nav-item">
               <a className="nav-link active" id="editor-tab" data-toggle="tab" href="#editor" role="tab" aria-controls="editor" aria-selected="true">Редактор</a>
@@ -18,8 +18,8 @@ const App = () => (
               <a className="nav-link" id="output-tab" data-toggle="tab" href="#output" role="tab" aria-controls="output" aria-selected="false">Вывод</a>
             </li>
           </ul>
-          <div className="tab-content" id="myTabContent">
-            <div className="tab-pane fade active show" id="editor" role="tabpanel" aria-labelledby="editor-tab">
+          <div className="tab-content h-100" id="myTabContent">
+            <div className="tab-pane fade active show h-100" id="editor" role="tabpanel" aria-labelledby="editor-tab">
               <Editor />
             </div>
             <div className="tab-pane fade" id="tests" role="tabpanel" aria-labelledby="tests-tab">
