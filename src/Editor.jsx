982159fc43b90c8e878ff9f03d273aea18a07d38
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-22 17:24:51 +0300

    Add(src/Editor.jsx) autofocus to editor

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -8,14 +8,14 @@ import 'codemirror/keymap/sublime.js';
 
 const Editor = () => {
   const initialValue = `(cond ((= a 4) 6)
-      ((= b 4) (+ 6 7 a))
-      (else 25))
-    (+ 2 (if (> b a) b a))
-    (* (cond ((> a b) a)
-        ((< a b) b)
-        (else -1))
-    (+ a 1))
-  `;
+    ((= b 4) (+ 6 7 a))
+    (else 25))
+  (+ 2 (if (> b a) b a))
+  (* (cond ((> a b) a)
+      ((< a b) b)
+      (else -1))
+  (+ a 1))
+`;
 
   return (
     <CodeMirrorEditor
@@ -26,9 +26,10 @@ const Editor = () => {
         lineNumbers: true,
         tabSize: 2,
         keyMap: 'sublime',
+        autofocus: true,
       }}
       onChange={(editor, data, value) => {
-        console.log(value);
+        // console.log(value);
       }}
     />
   );
