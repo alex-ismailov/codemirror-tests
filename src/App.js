891326f5571697a3c9d0be16caf7dbc881249e03
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-23 00:01:30 +0300

    Remove(src/App.js) backup comment

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => (
                 <Nav.Link eventKey="output">Вывод</Nav.Link>
               </Nav.Item>
             </Nav>
-            <Tab.Content className='h-100 overflow-auto'>
+            <Tab.Content className='h-100 overflow-auto bg-success'>
               <Tab.Pane eventKey="editor" className='h-100'>
                 <Editor />
               </Tab.Pane>
@@ -39,37 +39,3 @@ const App = () => (
 );
 
 export default App;
-
-// Шаблон на native bootstrap 4.6
-// const App = () => (
-//   <div className='container'>
-//     <div className='vh-100'>
-//       <div className='row h-75 justify-content-center'>
-//         <div className="col-6 card px-0 mt-5">
-//           <ul className="nav nav-tabs justify-content-center" id="myTab" role="tablist">
-//             <li className="nav-item">
-//               <a className="nav-link active" id="editor-tab" data-toggle="tab" href="#editor" role="tab" aria-controls="editor" aria-selected="true">Редактор</a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link" id="tests-tab" data-toggle="tab" href="#tests" role="tab" aria-controls="tests" aria-selected="false">Тесты</a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link" id="output-tab" data-toggle="tab" href="#output" role="tab" aria-controls="output" aria-selected="false">Вывод</a>
-//             </li>
-//           </ul>
-//           <div className="tab-content h-100" id="myTabContent">
-//             <div className="tab-pane fade active show h-100" id="editor" role="tabpanel" aria-labelledby="editor-tab">
-//               <Editor />
-//             </div>
-//             <div className="tab-pane fade" id="tests" role="tabpanel" aria-labelledby="tests-tab">
-//               Результат тестов
-//             </div>
-//             <div className="tab-pane fade" id="output" role="tabpanel" aria-labelledby="output-tab">
-//               Вывод
-//             </div>
-//           </div>
-//         </div>      
-//       </div>
-//     </div>
-//   </div>
-// );
