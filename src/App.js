9195ada8f626c7f1cb8a7887a5c535163e5a37c0
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-22 19:02:25 +0300

    Refactor(src/App.js) Col height, add border to Col

--- src/App.js
+++ src/App.js
@@ -1,41 +1,76 @@
 // import CodeBasicEditor from './CodeBasicEditor.jsx';
 import Editor from './Editor.jsx';
-import { Container, Row, Col, Nav, Tab } from 'react-bootstrap';
+import { Container, Row, Col, Card, Nav, Tab } from 'react-bootstrap';
 // import LivewireTemp from './LivewireTemp.jsx'
 
+// const App = () => (
+//   <Container>
+//     <div className='vh-100'>
+//       <Row className='h-75 justify-content-center'>
+//         <Col xs={6} className='card h-100 px-0 mt-5'>
+//           <Tab.Container defaultActiveKey="editor">
+//             <Nav variant='tabs'>
+//               <Nav.Item>
+//                 <Nav.Link eventKey="editor">Редактор</Nav.Link>
+//               </Nav.Item>
+//               <Nav.Item>
+//                 <Nav.Link eventKey="tests">Тесты</Nav.Link>
+//               </Nav.Item>
+//               <Nav.Item>
+//                 <Nav.Link eventKey="output">Вывод</Nav.Link>
+//               </Nav.Item>
+//             </Nav>
+//             <Tab.Content className='h-100'>
+//               <Tab.Pane eventKey="editor" className='h-100'>
+//                 <Editor />
+//               </Tab.Pane>
+//               <Tab.Pane eventKey="tests">
+//                 Результат тестов
+//               </Tab.Pane>
+//               <Tab.Pane eventKey="output">
+//                 Вывод
+//               </Tab.Pane>
+//             </Tab.Content>
+//           </Tab.Container>
+//         </Col>
+//       </Row>
+//     </div>
+//   </Container>
+// );
+
 const App = () => (
-  <Container>
+  <Container className=''>
     <div className='vh-100'>
       <Row className='h-75 justify-content-center'>
-          <Col xs={6} className='card px-0 mt-5'>
-            <Tab.Container defaultActiveKey="editor">
-              <Nav variant='tabs'>
-                <Nav.Item>
-                  <Nav.Link eventKey="editor">Редактор</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="tests">Тесты</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="output">Вывод</Nav.Link>
-                </Nav.Item>
-              </Nav>
-              <Tab.Content className='h-100'>
-                <Tab.Pane eventKey="editor" className='h-100'>
-                  <Editor />
-                </Tab.Pane>
-                <Tab.Pane eventKey="tests">
-                  Результат тестов
-                </Tab.Pane>
-                <Tab.Pane eventKey="output">
-                  Вывод
-                </Tab.Pane>
-              </Tab.Content>
-            </Tab.Container>
-          </Col>
-        </Row>
-      </div>
-    </Container>
+        <Col xs={6} className='h-100 border overflow-hidden px-0 mt-5'>
+          <Tab.Container defaultActiveKey="editor" className='border'>
+            <Nav variant='tabs'>
+              <Nav.Item>
+                <Nav.Link eventKey="editor">Редактор</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="tests">Тесты</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="output">Вывод</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content className='h-100'>
+              <Tab.Pane eventKey="editor" className='h-100'>
+                <Editor />
+              </Tab.Pane>
+              <Tab.Pane eventKey="tests">
+                Результат тестов
+              </Tab.Pane>
+              <Tab.Pane eventKey="output">
+                Вывод
+              </Tab.Pane>
+            </Tab.Content>
+          </Tab.Container>
+        </Col>
+      </Row>
+    </div>
+  </Container>
 );
 
 export default App;
